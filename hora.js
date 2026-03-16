8327e8593865396ed3367430fff00622c5c51a49
Oliveira612 <oliveira.almeida.arthur@escola.pr.gov.br>
2026-03-16 13:51:40 +0000

    versao1

--- hora.js
+++ hora.js
@@ -5,3 +5,11 @@ let horasTrabalhadas = 8;
 
 console.log("--- Resumo do Expediente---");
 console.log(`Horas Iniciais: ${horasTrabalhadas}`);
+
+//No dia seguinte ele precisou sair uma hora mais cedo para ir ao médico
+horasTrabalhadas--
+console.log(`Após saída antecipada (horasTrabalhadas--): ${horasTrabalhadas}h`);
+
+//Cálculo final
+let taxaSistema = 15;
+let pagamentoTotal = (horasTrabalhadas * valorPorHora) - taxaSistema;
