0c0cdf7e1de3ccf8f608343a7a5efb65f20ef02b
Oliveira612 <oliveira.almeida.arthur@escola.pr.gov.br>
2026-03-23 13:46:48 +0000

    version4

--- temperatura.js
+++ temperatura.js
@@ -1,7 +1,7 @@
 //Conversão de temperaturas
 
 const cidade = "Curitiba";
-const tempCelsius = 18.5;
+const tempCelsius = 23;
 
 const tempFahrenheit = tempCelsius * (9 / 5) + 32;
 const tempKelvin = tempCelsius + 273.15;
@@ -9,5 +9,5 @@ const variacaoDiaria = 26.3 - tempCelsius
 
 console.log(`Temperatura em ${cidade} hoje está ${tempCelsius}.`);
 console.log(`Celsius: ${tempCelsius}`);
-
-//Fazer console.log para Fahrenheit e para Kelvin
+console.log(`Kelvin: ${tempKelvin} Kº`);
+console.log(`\nVariação prevista: ${variacaoDiaria.toFixed(1)} Cº`);
